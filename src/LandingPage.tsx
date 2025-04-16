cfe376149c6c623fbb6703c2f422ac0905f698bd
Maduabuchi Chibuike <juniortechbro@gmail.com>
2025-04-16 03:39:36 +0100

    refactor

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,11 +1,13 @@
+import BestSellers from "./components/BestSellers/BestSellers";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
 export default function LandingPage() {
 	return (
-		<div>
+		<>
 			<Navbar />
 			<Hero />
-		</div>
+			<BestSellers />
+		</>
 	);
 }
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,16 @@
 import styles from "./Hero.module.css";
 
 export default function Hero() {
-	return <section className={styles.section}>sds</section>;
+	return (
+		<section className={styles.section}>
+			<div className="w-full max-w-[76.5rem] mx-auto">
+				<div className=" pt-[18.75rem]">
+					<h1 className="font-primary italic text-[34px] w-full max-w-[295px] leading-[1.8]">
+						Elegance in simplicity, Earth’s harmony
+					</h1>
+					<button className="bg-white px-[67px] py-[0.5rem] mt-[27px]">New In</button>
+				</div>
+			</div>
+		</section>
+	);
 }
